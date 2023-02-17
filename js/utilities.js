8c2139506be0c60aa20c23aa9269edafb7f30a71
atiqur <atiqurrahman.cse2552@gmail.com>
2023-02-17 15:55:12 +0600

    update

--- js/utilities.js
+++ js/utilities.js
@@ -1,4 +1,9 @@
 
+// document.getElementById('remove_item').addEventListener('click',function(event){
+//     const deleteElement=event.target.parentNode.parentNode
+//     deleteElement.remove(event.target.parentNode)
+// })
+
 function phoneAndPhoneCaseItemUpdate(incrementDecrement,phoneAndItemById){
     const phoneAndPhoneCaseTotalItemsElement=document.getElementById(phoneAndItemById)
     const previousPhoneOrCaseItem=parseInt(phoneAndPhoneCaseTotalItemsElement.value)
@@ -25,13 +30,32 @@ function phone_phoneCaseTotalPriceUpdate(quantity,priceElementId,defaultPrice){
 
 }
 
+// setTextElementValueById
+function setTextElementValueById(elementId, value){
+    const subTotalElement = document.getElementById(elementId);
+    subTotalElement.innerText = value;
+}
 
 function subTotal(){
-    const subTotalElement=document.getElementById('subTotalPrice')
-    const phoneTotalPrice=document.getElementById('totalPhonePriceElement')
-    const phone_caseTotalPrice=document.getElementById('totalPhoneCasePriceElement')
-    const phonePrice=parseInt(phoneTotalPrice.innerText) 
-    const casePrice=parseInt(phone_caseTotalPrice.innerText)
-    const subTotal=phonePrice+casePrice
-    subTotalElement.innerText=subTotal
+
+    const phoneTotalPriceElement=document.getElementById('totalPhonePriceElement')
+    const phone_caseTotalPriceElement=document.getElementById('totalPhoneCasePriceElement')
+
+    const phonePrice=parseInt(phoneTotalPriceElement.innerText) 
+    const casePrice=parseInt(phone_caseTotalPriceElement.innerText)
+
+    const currentSubTotal=phonePrice+casePrice
+
+    setTextElementValueById('subTotalPrice',currentSubTotal)
+
+    // calculate tax
+    console.log(currentSubTotal)
+    const taxAmountString = (currentSubTotal * 0.1).toFixed(2);
+    const taxAmount = parseFloat(taxAmountString);
+    setTextElementValueById('tax-amount', taxAmount);
+    // final total
+    const finalAmount = currentSubTotal + taxAmount;
+    setTextElementValueById('final-total', finalAmount);
 }
+
+subTotal()
